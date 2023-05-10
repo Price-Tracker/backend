5502838adf37fba55226123728df7fd3d29c6bde
ElXreno <elxreno@gmail.com>
2023-05-10 12:29:32 +0300

    flake: Init

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, rust-overlay, gitignore }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        naersk-lib = pkgs.callPackage naersk {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        };
+
+        backend = naersk-lib.buildPackage {
+          inherit nativeBuildInputs buildInputs;
+          pname = "backend";
+          src = gitignore.lib.gitignoreSource ./.;
+        };
+
+        nativeBuildInputs = with pkgs; [ clang ];
+        buildInputs = with pkgs; [ postgresql.lib ];
+      in
+      {
+        packages.backend = backend;
+        packages.default = self.packages.${system}.backend;
+
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = nativeBuildInputs ++ [
+            diesel-cli
+          ];
+          buildInputs = buildInputs ++ [
+            (rust-toolchain.override { extensions = [ "rust-src" ]; })
+          ];
+        };
+
+        checks = {
+          backend = self.packages.${system}.default.overrideAttrs (super: { doCheck = true; });
+        };
+      });
+
+  nixConfig = {
+    extra-experimental-features = "nix-command flakes";
+    extra-substituters = [ "https://elxreno-rust.cachix.org" ];
+    extra-trusted-public-keys = [ "elxreno-rust.cachix.org-1:cfUElkBCai6A6hqku/tOCrYt9qF+vQtAV8+8MF16gf8=" ];
+  };
+}
