44ffd5f3dbc531242b52aed6b12d6b732011a38a
ElXreno <elxreno@gmail.com>
2023-05-15 19:22:40 +0300

    Return success on signup

--- src/api/account_controller.rs
+++ src/api/account_controller.rs
@@ -17,7 +17,7 @@ use deadpool_diesel::postgres::Pool;
 #[post("/signup")]
 pub async fn signup(user_dto: web::Json<UserDTO>, pool: web::Data<Pool>) -> Result<HttpResponse> {
     match account_service::signup(user_dto.0, &pool).await {
-        Ok(message) => Ok(HttpResponse::Ok().json(ResponseBody::new(&message, ""))),
+        Ok(_) => Ok(HttpResponse::Ok().json(ResponseBody::new("success", ""))),
         Err(err) => Ok(err.response()),
     }
 }
